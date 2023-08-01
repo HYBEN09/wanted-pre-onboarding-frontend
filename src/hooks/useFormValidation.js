82076d7372f87f81fd02dbb98527e1a6b67a2c58
hyebeen byun <hyebeen2658@naver.com>
2023-08-01 15:34:35 +0900

    [feat] validation 기능 hook으로 분리

---
+++ src/hooks/useFormValidation.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const useFormValidation = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateEmail = () => {
+    if (!email.includes('@')) {
+      setEmailError('이메일 형식이 올바르지 않습니다.');
+      return false;
+    } else {
+      setEmailError('');
+      return true;
+    }
+  };
+
+  const validatePassword = () => {
+    if (password.length < 8) {
+      setPasswordError('비밀번호는 8자 이상이어야 합니다.');
+      return false;
+    } else {
+      setPasswordError('');
+      return true;
+    }
+  };
+
+  return {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    emailError,
+    passwordError,
+    validateEmail,
+    validatePassword,
+  };
+};
+
+export default useFormValidation;
