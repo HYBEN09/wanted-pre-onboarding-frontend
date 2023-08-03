c5e89508887900ac3adb03aea8053253c00c81e7
hyebeen byun <hyebeen2658@naver.com>
2023-08-03 22:38:30 +0900

    [refactor] API 설정 로직을 별도의 파일로 분리후 적용

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 export const BASE_URL = 'https://www.pre-onboarding-selection-task.shop/';
+
+export const createApiInstance = (baseURL, headers) => {
+  const token = localStorage.getItem('token');
+  return axios.create({
+    baseURL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+      ...headers,
+    },
+  });
+};
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,51 +1,40 @@
-import axios from 'axios';
-import { BASE_URL } from './api';
+import { createApiInstance, BASE_URL } from './api';
 
-const api = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+const api = createApiInstance(BASE_URL);
 
-// signUp
-export const signUp = async (email, password) => {
+const makeRequest = async (url, data) => {
   try {
-    const response = await api.post('auth/signup', {
-      email,
-      password,
-    });
-
-    if (response.status === 201) {
-      return { success: true };
-    } else {
-      console.error('회원가입 실패:', response);
-      return { success: false, error: response };
-    }
+    const response = await api.post(url, data);
+    return {
+      success: response.status === 201 || response.status === 200,
+      data: response.data,
+    };
   } catch (error) {
-    console.error('회원가입 중 오류가 발생:', error);
+    console.error('오류가 발생:', error);
     return { success: false, error };
   }
 };
 
+// signUp
+export const signUp = async (email, password) => {
+  const data = {
+    email,
+    password,
+  };
+  return makeRequest('auth/signup', data);
+};
+
 // signIn
 export const signIn = async (email, password) => {
-  try {
-    const response = await api.post('auth/signin', {
-      email,
-      password,
-    });
+  const data = {
+    email,
+    password,
+  };
 
-    if (response.status === 200) {
-      const token = response.data.access_token;
-      window.localStorage.setItem('token', token);
-      return { success: true };
-    } else {
-      console.error('로그인 실패:', response);
-      return { success: false, error: response };
-    }
-  } catch (error) {
-    console.error('로그인 중 오류가 발생:', error);
-    return { success: false, error };
+  const result = await makeRequest('auth/signin', data);
+  if (result.success) {
+    const token = result.data.access_token;
+    window.localStorage.setItem('token', token);
   }
+  return result;
 };
