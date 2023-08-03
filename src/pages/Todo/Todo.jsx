13304dd66419ff017c58b79709babce403bbf956
hyebeen byun <hyebeen2658@naver.com>
2023-08-04 07:43:10 +0900

    [feat] ToDo component 구현: API 호출 및 상태 업데이트 최적화

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,5 +1,69 @@
-function Todo() {
-  return <div>Todo</div>;
-}
+import {
+  createTodo,
+  deleteTodo,
+  fetchTodo,
+  updateTodo,
+} from '../../api/todoConfig';
+import classes from './Todo.module.css';
+import { useCallback, useEffect, useState } from 'react';
+import CreateTodo from '../../components/TodoItem/CreateTodo';
+import TodoItem from '../../components/TodoItem/TodoItem';
 
-export default Todo;
+const ToDo = () => {
+  const [todoInput, setTodoInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = async () => {
+    await createTodo(todoInput);
+    setTodoInput('');
+    fetchTodoList();
+  };
+
+  const fetchTodoList = async () => {
+    const response = await fetchTodo();
+    setTodoList(response.data);
+  };
+
+  const removeTodo = useCallback(async (todo) => {
+    await deleteTodo(todo.id);
+    fetchTodoList();
+  }, []);
+
+  const handleCheckToggle = useCallback(async (todo) => {
+    await updateTodo(todo.id, todo.todo, !todo.isCompleted);
+    fetchTodoList();
+  }, []);
+
+  const handleTodoInputChange = (e) => {
+    setTodoInput(e.target.value);
+  };
+
+  useEffect(() => {
+    fetchTodoList();
+  }, []);
+
+  return (
+    <div className={classes.todoList}>
+      <div className={classes.todoHeader}>TODO LIST</div>
+      <CreateTodo
+        onSubmit={addTodo}
+        todo={todoInput}
+        onChange={handleTodoInputChange}
+      />
+      <ul className={classes.ul}>
+        {todoList.map((todo) => (
+          <li key={todo.id} className={classes.list}>
+            <TodoItem
+              todo={todo}
+              removeTodo={removeTodo}
+              getTodo={fetchTodoList}
+              isChecked={handleCheckToggle}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDo;
